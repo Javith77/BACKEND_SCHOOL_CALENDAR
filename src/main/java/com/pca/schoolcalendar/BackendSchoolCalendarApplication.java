eb836b76250d2037885dbb33ee6dbf0c6c14104b
Javith77 <javimemo97@gmail.com>
2022-10-16 15:09:40 -0500

    Add base project

---
+++ src/main/java/com/pca/schoolcalendar/BackendSchoolCalendarApplication.java
@@ -0,0 +1,13 @@
+package com.pca.schoolcalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSchoolCalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSchoolCalendarApplication.class, args);
+	}
+
+}
