ddefbf52ce487810c53f7cc3048d1a79639bfd32
Javith77 <javimemo97@gmail.com>
2022-10-16 17:13:02 -0500

    Create response object

---
+++ src/main/java/com/pca/schoolcalendar/response/ErrorResponse.java
@@ -0,0 +1,12 @@
+package com.pca.schoolcalendar.response;
+
+import java.util.Date;
+
+public class ErrorResponse {
+
+    private int statusCode;
+    private Date timestamp;
+    private String message;
+    private String description;
+
+}
---
+++ src/main/java/com/pca/schoolcalendar/response/MessageResponse.java
@@ -0,0 +1,12 @@
+package com.pca.schoolcalendar.response;
+
+import java.util.Date;
+
+public class MessageResponse {
+
+    private int statusCode;
+    private Date timestamp;
+    private String message;
+    private Object data;
+
+}
