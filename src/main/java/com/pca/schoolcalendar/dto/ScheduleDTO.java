0509d8865548746ec43b8c04fec3ebafea19bb3b
Javith77 <javimemo97@gmail.com>
2022-10-16 17:23:49 -0500

    Refactor DTO

--- src/main/java/com/pca/schoolcalendar/dto/ScheduleDTO.java
+++ src/main/java/com/pca/schoolcalendar/dto/ScheduleDTO.java
@@ -5,9 +5,11 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
-public class SheduleDTO {
+public class ScheduleDTO {
 
     private Integer id;
+
+    private Integer idCourse;
 
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     @NotNull
